341a91c3b19a7131ca55c07ebedb56a52046716e
gaddamkiran <117713136+gaddamkiran@users.noreply.github.com>
2022-11-08 12:28:58 +0530

    Add files via upload

---
+++ Student/src/main/java/com/xyz/api/repo/StudentRepository.java
@@ -0,0 +1,12 @@
+package com.xyz.api.repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.xyz.api.model.Student;
+
+@Repository
+
+public interface StudentRepository extends JpaRepository<Student, Integer>  {
+
+}
